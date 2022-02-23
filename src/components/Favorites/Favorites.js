c5ca2f9084cb0b99fbaf0523020c4ec9ee7adf1e
alexander <urazaev.alexander@gmail.com>
2022-02-23 15:07:41 +0500

    favorites.js requests

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,50 +1,79 @@
 import React, {Component} from 'react';
 import './Favorites.css';
 import {connect} from 'react-redux';
-import { delFav } from '../../redux/actions'
+import {delFav} from '../../redux/actions'
 
 
 class Favorites extends Component {
- 
-saveList = () => {
-  fetch('https://acb-api.algoritmika.org/api/movies/list', {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify({
-      "title": "Matrix",
-      "movies": [
-          "tt0068646",
-          "tt0098019"
-      ]})
-  })
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-  })
-  .catch(error => {
-    console.log('Произошла ошибка')
-  })
-}
+
+  state = {
+    // это приходит с сервера
+    savedLists: []
+  }
+
+  filmIds = this.props.favMovies.map((item) => item.imdbID)
+
+  saveList = () => {
+    fetch('https://acb-api.algoritmika.org/api/movies/list', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      // отдаем серверу
+      body: JSON.stringify({
+        "title": "Matrix",
+
+        // TODO: так можно ли делать? Что у нас сейчас в this.filmIds?
+        "movies": this.filmIds
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.id)
+        console.log(data.title)
+
+        // TODO: сделать так чтобы данные записывались не поверх старого массива а дополняли его
+        // желательно в конец массива который в стейте (как обновить массив в стейте правильно?)
+
+        this.setState(
+          {savedLists: [{id: data.id, name: data.title}]}
+        )
+
+        console.log(this.state)
+
+      })
+      .catch(error => {
+        console.log('Произошла ошибка')
+      })
+  }
+
+
   render() {
+
+
     return (
       <div className="favorites">
         <input defaultValue={'Новый список'} className="favorites__name"/>
         <ul className="favorites__list">
           {this.props.favMovies.map((item) => {
-            return <li key={item.imdbID}>{item.Title} ({item.Year}) 
-            <button onClick={()=> this.props.delFav(item.imdbID)} 
-            className="favorites__delete" 
-            type="button">X
-            </button></li>;
+            return <li key={item.imdbID}>{item.Title} ({item.Year})
+              <button onClick={() => this.props.delFav(item.imdbID)}
+                      className="favorites__delete"
+                      type="button">X
+              </button></li>;
           })}
         </ul>
         <button onClick={() => this.saveList()} type="button" className="favorites__save">Сохранить список</button>
+
+        {this.state.savedLists.map.length > 0 &&
+        this.state.savedLists.map(({id, name}) => (<p>
+          <a href={`/list/${id}`} target={'_blank'}>Открыть список: {name}</a>
+        </p>))}
       </div>
     );
   }
 }
+
 const mapDispatchToProps = dispatch => ({
   delFav: (id) => dispatch(delFav(id))
 })
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,31 +1,6 @@
 const initialState = {
   allMovies: {
-    Search: [
-      // matrix
-      // fetch results
-      {
-        Title: "The Matrix",
-        Year: "1999",
-        imdbID: "tt0133093",
-        Type: "movie",
-        Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
-      },
-      {
-        Title: "The Matrix Reloaded",
-        Year: "2003",
-        imdbID: "tt0234215",
-        Type: "movie",
-        Poster: "https://m.media-amazon.com/images/M/MV5BODE0MzZhZTgtYzkwYi00YmI5LThlZWYtOWRmNWE5ODk0NzMxXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
-      },
-      {
-        Title: "The Matrix Revolutions",
-        Year: "2003",
-        imdbID: "tt0242653",
-        Type: "movie",
-        Poster: "https://m.media-amazon.com/images/M/MV5BNzNlZTZjMDctZjYwNi00NzljLWIwN2QtZWZmYmJiYzQ0MTk2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-      }
-
-    ]
+    Search: []
   },
   favMovies: [
     {
@@ -35,9 +10,23 @@ const initialState = {
       Type: "movie",
       Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
     },
+    {
+      Title: "The Matrix",
+      Year: "1999",
+      imdbID: "tt0133095",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
+    },
+    {
+      Title: "The Matrix",
+      Year: "1999",
+      imdbID: "tt0138093",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
+    },
   ],
   inputValue: '',
-  
+
 }
 
 function reducer(state = initialState, action) {
@@ -66,13 +55,13 @@ function reducer(state = initialState, action) {
         favMovies: [...state.favMovies, {...newItem}]
       }
 
-      case 'DEL_FAV':
-        console.log(action.payload.data)
-        const newFav = state.favMovies.filter((item) => item.imdbID !== action.payload.data)
-        return {
-          ...state,
-          favMovies: newFav
-        }
+    case 'DEL_FAV':
+      console.log(action.payload.data)
+      const newFav = state.favMovies.filter((item) => item.imdbID !== action.payload.data)
+      return {
+        ...state,
+        favMovies: newFav
+      }
     default:
       return state;
   }
